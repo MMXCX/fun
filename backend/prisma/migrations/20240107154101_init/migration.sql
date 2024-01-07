30d35318c25d8d5564b3e111b177b9e3c266a9c0
Max Bell <markmain@mail.ru>
2024-01-07 18:44:48 +0300

    07.01.2024 new

--- backend/prisma/migrations/20240107154101_init/migration.sql
+++ backend/prisma/migrations/20240107154101_init/migration.sql
@@ -11,8 +11,20 @@ CREATE TABLE "User" (
     CONSTRAINT "User_pkey" PRIMARY KEY ("id")
 );
 
+-- CreateTable
+CREATE TABLE "Token" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "refreshToken" TEXT NOT NULL,
+
+    CONSTRAINT "Token_pkey" PRIMARY KEY ("id")
+);
+
 -- CreateIndex
 CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- CreateIndex
 CREATE UNIQUE INDEX "User_activation_uuid_key" ON "User"("activation_uuid");
+
+-- AddForeignKey
+ALTER TABLE "Token" ADD CONSTRAINT "Token_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
